a40ccda8216e7ec2cc7ad9adf28684783cba5c76
vinaykumarchennupati <vinaykumarchennupati@192.168.1.4>
2021-09-12 16:55:47 -0500

    First commit...

---
+++ src/Samplecode.java
@@ -0,0 +1,15 @@
+
+public class Samplecode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		String hungry="Yes";
+		if(hungry=="Yes")
+			System.out.println("Eat something....");
+		else {
+			System.out.println("Do work...");
+		}
+	}
+
+}
